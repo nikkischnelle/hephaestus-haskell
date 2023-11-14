9a3ff1e5647b01c82c9abb9970468a91ce2dff6c
nikkischnelle <nikki@tschierske.com>
2023-11-14 03:57:47 +0100

    implement basic file menu

--- Main.hs
+++ Main.hs
@@ -15,6 +15,13 @@ import Text.Pandoc (ReaderOptions)
 import Text.Pandoc (ReaderOptions(readerExtensions))
 import Text.Pandoc.Extensions
 import Text.Blaze.Html.Renderer.Pretty (renderHtml)
+import System.Directory (listDirectory)
+import System.FilePath ((</>))
+import Control.Monad (forM)
+import System.Posix (getFileStatus)
+import System.Posix.Files (isDirectory)
+import System.FilePath.Posix (dropExtension)
+import Data.Foldable (forM_)
 
 readerOptions :: ReaderOptions
 readerOptions = def {
@@ -36,17 +43,49 @@ main = scotty 3000 $ do
         do
             fileContent <- liftIO $ readFile (mconcat [".", beam, ".md"])
 
+            dirContents <- liftIO $ traverseDir "." (T.isPrefixOf "./." . T.pack)
+
+            let markdownList = [drop 1 $ dropExtension x | x <- dirContents, T.isSuffixOf ".md" $ T.pack x]
+
             h <- liftIO $ mdToHtml $ T.pack fileContent
             
-            let page = createPage h
+            let page = createPage h markdownList
             Web.Scotty.html $ LT.pack $ renderHtml page
 
-createPage :: Html -> Html
-createPage text = docTypeHtml $ do
+traverseDir :: FilePath -> (FilePath -> Bool) -> IO [FilePath]
+traverseDir top exclude = do
+  subPaths <- listDirectory top
+  let actualSubPaths = [top </> subPath | subPath <- subPaths]
+  (filePaths, dirPaths) <- partitionM isFile actualSubPaths
+  paths <- forM (filter (not.exclude) dirPaths) $ \subPath -> do
+    let path = subPath
+    traverseDir path exclude
+  return (filePaths ++ concat paths)
+
+isFile :: FilePath -> IO Bool
+isFile path = do
+  fileStatus <- getFileStatus path
+  return $ not $ isDirectory fileStatus
+
+partitionM :: (Monad m) => (a -> m Bool) -> [a] -> m ([a], [a])
+partitionM _ [] = return ([], [])
+partitionM p (x:xs) = do
+  test <- p x
+  (ys, zs) <- partitionM p xs
+  return $ if test then (x:ys, zs) else (ys, x:zs)
+
+createPage :: Html -> [String] -> Html
+createPage text list = docTypeHtml $ do
   H.head $ do
     H.title "Hephaestus"
   H.body $ do
+    explorer list
     toHtml text
+
+explorer :: [String] -> Html
+explorer list = H.div ! class_ "explorer" $ do
+  ul $ forM_ list $ \filepath ->
+    li $ a ! href (toValue filepath) $ toHtml filepath
 
 mdToHtml :: Text -> IO Html
 mdToHtml markdown = do
