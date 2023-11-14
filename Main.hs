50ff05344a01517b31ce9c52f369bb82035eafd8
nikkischnelle <nikki@tschierske.com>
2023-11-14 03:45:52 +0100

    serve files if requested with file ending

--- Main.hs
+++ Main.hs
@@ -4,5 +4,9 @@ import Web.Scotty
 
 main :: IO ()
 main = scotty 3000 $ do
-  get "/" $ do
-    Web.Scotty.html "<h1>root</h1>"
+    get "/" $ do
+        Web.Scotty.html "<h1>root</h1>"
+
+    get (regex "^.*\\.(.*)$") $ do
+        beam <- captureParam "0"
+        file $ mconcat [".", beam]
