79bf4c065b8a55199a076f6a90e9ecf50d968564
nikkischnelle <nikki@tschierske.com>
2023-11-16 01:20:57 +0100

    add open folder icon

--- web/script.js
+++ web/script.js
@@ -1,8 +1,11 @@
 let directoryList;
+let collapsibleList;
 
 document.addEventListener("DOMContentLoaded", function(){
     directoryList = document.getElementsByClassName("collapsible_content");
-    const storedStyles = JSON.parse(localStorage.getItem('directoryStates'));
+    collapsibleList = document.getElementsByClassName("collapsible");
+
+    const storedStyles = JSON.parse(localStorage.getItem('collapsibleStates'));
     if (storedStyles != null) {
         for (let i = 0; i < directoryList.length; i++) {
             const element = directoryList[i];
@@ -10,18 +13,30 @@ document.addEventListener("DOMContentLoaded", function(){
         }
     }
 
-    var coll = document.getElementsByClassName("collapsible");
-    var i;
+    const iconStyles = JSON.parse(localStorage.getItem('iconStates'));
+    if (iconStyles != null) {
+        for (let i = 0; i < collapsibleList.length; i++) {
+            const element = collapsibleList[i];
+            element.innerHTML = iconStyles[i];
+        }
+    }
 
-    for (i = 0; i < coll.length; i++) {
-        coll[i].addEventListener("click", function() {
+
+    for (var i = 0; i < collapsibleList.length; i++) {
+        collapsibleList[i].addEventListener("click", function() {
             var content = this.nextElementSibling;
+            var icon = this.children[0]
+
             if (content.style.display === "block") {
                 content.style.display = "none";
+                icon.innerHTML = "folder"
             } else {
                 content.style.display = "block";
+                icon.innerHTML = "folder_open"
             }
-            saveDirectoryStates();
+
+            saveCollapsibleStates();
+            saveIconStates();
         });
     } 
 });
@@ -30,11 +45,20 @@ function redirect(url) {
     location.replace(url)
 }
 
-function saveDirectoryStates() {
+function saveCollapsibleStates() {
     let styleList = new Array();
     for (let i = 0; i < directoryList.length; i++) {
         const element = directoryList[i];
         styleList[i] = element.style.display;
     }
-    localStorage.setItem('directoryStates', JSON.stringify(styleList));
+    localStorage.setItem('collapsibleStates', JSON.stringify(styleList));
+}
+
+function saveIconStates() {
+    let iconStateList = new Array();
+    for (let i = 0; i < collapsibleList.length; i++) {
+        const element = collapsibleList[i];
+        iconStateList[i] = element.innerHTML;
+    }
+    localStorage.setItem('iconStates', JSON.stringify(iconStateList));
 }
